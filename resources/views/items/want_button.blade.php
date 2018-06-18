ca613d22e4631849bda218e179b861f8e080212d
tatsuma.kato <ec2-user@ip-172-31-22-12.us-east-2.compute.internal>
2018-06-18 04:10:07 +0000

    have

--- resources/views/items/want_button.blade.php
+++ resources/views/items/want_button.blade.php
@@ -1,11 +1,11 @@
 @if (Auth::user()->is_wanting($item->code))
     {!! Form::open(['route' => 'item_user.dont_want', 'method' => 'delete']) !!}
         {!! Form::hidden('itemCode', $item->code) !!}
-        {!! Form::submit('Want', ['class' => 'btn btn-success']) !!}
+        {!! Form::submit('Not Want', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
 @else
     {!! Form::open(['route' => 'item_user.want']) !!}
         {!! Form::hidden('itemCode', $item->code) !!}
-        {!! Form::submit('Want it', ['class' => 'btn btn-primary']) !!}
+        {!! Form::submit('Want', ['class' => 'btn btn-primary']) !!}
     {!! Form::close() !!}
 @endif
